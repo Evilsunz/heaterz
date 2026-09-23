48537ec188ede27b4f99ba52da34b798dd94b956
Maxim <maxim_dmytrenko@comcast.com>
2026-09-24 02:31:49 +0300

    Changes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::wifi::{ClientConfiguration, Configuration, EspWifi, };
 use esp_idf_svc::mdns::EspMdns;
 use esp_idf_svc::io::EspIOError;
+use esp_idf_svc::sntp::{EspSntp, SyncStatus};
+use serde::{Deserialize, Serialize};
+use std::fs::{File, OpenOptions};
+use std::io::BufReader;
+use std::path::Path;
+use std::time::SystemTime;
+
+static FILE_PATH: &str = "/littlefs/log.csv";
 
 fn main() -> anyhow::Result<()> {
     link_patches();
@@ -44,7 +52,7 @@ fn main() -> anyhow::Result<()> {
     wifi.set_configuration(&Configuration::Client(
         ClientConfiguration {
             ssid: heapless::String::try_from("USG Ishimura").unwrap(),
-            password: heapless::String::try_from("pswd").unwrap(),
+            password: heapless::String::try_from("pwd").unwrap(),
             ..Default::default()
         },
     ))?;
@@ -56,42 +64,36 @@ fn main() -> anyhow::Result<()> {
 
     println!("WiFi connected");
 
+    let _sntp = EspSntp::new_default()?;
+    loop {
+        if _sntp.get_sync_status() == SyncStatus::Completed {
+            println!("Time is synced");
+            break;
+        }
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
+
     let mut mdns = EspMdns::take()?;
     mdns.set_hostname("heaterz")?;
     mdns.set_instance_name("HeaterZ Automation")?;
 
-    println!("Mdns connected + fqdn heaterz.local");
-    // use std::io::Write;
-    // let mut file = std::fs::OpenOptions::new()
-    //     .create(true)
-    //     .append(true)
-    //     .open("/littlefs/test.txt")
-    //     .unwrap();
-    // writeln!(file, "\nxxxxxxxVVVVVVVVV").unwrap();
-    //
-    // std::fs::write("/littlefs/test.txt", "hello world").unwrap();
-    // file.flush().unwrap();
+    println!("Mdns published + fqdn heaterz.local");
 
-    // std::fs::remove_file("/littlefs/test.txt").unwrap();
-
-    println!(
-        "exists = {}",
-        std::path::Path::new("/littlefs/test.txt").exists()
-    );
-
-    let text = std::fs::read_to_string("/littlefs/test.txt").unwrap();
-
-    println!("read: {}", text);
+    std::fs::remove_file(FILE_PATH).unwrap();
 
     let config = esp_idf_svc::http::server::Configuration::default();
     let mut server = EspHttpServer::new(&config)?;
 
     server.fn_handler("/", esp_idf_svc::http::Method::Get, |request| {
-        // Формируем простой HTML-ответ
+        if let Err(e) = read_log() {
+            println!("Error reading log file : {:?}", e);
+        }
+
+        // let text = std::fs::read_to_string(FILE_PATH).unwrap();
+        // println!("read file: {}", text);
         let html = r#"Heaterz !!!!"#;
         let mut response = request.into_ok_response()?;
         response.write(html.as_bytes())?;
-
         Ok::<(), EspIOError>(())
     })?;
 
@@ -100,39 +102,55 @@ fn main() -> anyhow::Result<()> {
         std::thread::sleep(std::time::Duration::from_secs(10));
         let ip_info = wifi.sta_netif().get_ip_info()?;
         println!("IP: {:?}", ip_info.ip);
+        let wattz = Wattz{
+            source: "priza1".to_string(),
+            wattz: 0,
+            date: get_current_timestamp(),
+        };
+        println!("Wattz: {:?}", wattz);
+        append_log(&wattz)?;
     }
 }
 
-// fn connect_wifi(
-//     modem: Modem,
-//     sysloop: EspSystemEventLoop,
-//     nvs: EspDefaultNvsPartition,
-// ) -> anyhow::Result<Box<EspWifi<'static>>> {
-//     let mut wifi = Box::new(EspWifi::new(modem, sysloop, Some(nvs))?);
-//
-//     wifi.set_configuration(&Configuration::Client(ClientConfiguration {
-//         ssid: heapless::String::try_from("USG Ishimura").unwrap(),
-//         password: heapless::String::try_from("illuminati").unwrap(),
-//         auth_method: AuthMethod::WPA2Personal,
-//         ..Default::default()
-//     }))?;
-//
-//     wifi.start()?;
-//
-//     println!("WiFi started");
-//
-//     wifi.connect()?;
-//
-//     println!("Connecting...");
-//
-//     while !wifi.is_connected()? {
-//         std::thread::sleep(std::time::Duration::from_millis(500));
-//     }
-//
-//     println!("WiFi connected");
-//
-//     let ip_info = wifi.sta_netif().get_ip_info()?;
-//     println!("IP: {:?}", ip_info.ip);
-//
-//     Ok(wifi)
-// }
+#[derive(Debug, Serialize, Deserialize)]
+struct Wattz {
+    source: String,
+    wattz: i16,
+    date: u32,
+}
+
+fn append_log(record: &Wattz) -> anyhow::Result<()> {
+    let file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(FILE_PATH)?;
+
+    let mut wtr = csv::WriterBuilder::new()
+        .has_headers(false)
+        .from_writer(file);
+    wtr.serialize(record)?;
+    wtr.flush()?;
+    Ok(())
+}
+
+fn read_log() -> anyhow::Result<()> {
+    let file = File::open(FILE_PATH)?;
+    let buf_reader = BufReader::new(file);
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        //.flexible(true)
+        .terminator(csv::Terminator::Any(b'\n'))
+        .from_reader(buf_reader);
+    for result in rdr.deserialize() {
+        let record: Wattz = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn get_current_timestamp() -> u32 {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs() as u32
+}
